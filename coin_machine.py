ad810809adde9ec1f3d808c3189761c9b0ec3ec8
Ubuntu <ubuntu@ip-172-31-21-184.eu-west-1.compute.internal>
2019-07-04 18:41:24 +0000

    more advanced coin machine

--- coin_machine.py
+++ coin_machine.py
@@ -1,15 +1,38 @@
 from byotest import *
 
-eur_coins = [100,50,20,10,5,2,1]
-usd_coins = [100,50,25,10,5,1]
+eur_coins = {
+    100 : 20,
+    50 : 20,
+    20 : 20,
+    10 : 20,
+    5 : 20,
+    2 : 20,
+    1 : 20
+}
+
+usd_coins = {
+    100 : 20,
+    50 : 20,
+    25 : 1,
+    10 : 1,
+    5 : 1,
+    1 : 3
+}
 
 def get_change(amount, denominations = eur_coins):
     change = []
     for coin in denominations:
-        while coin <= amount: 
-            change.append(coin)
-            amount -= coin
-    return change
+        while coin <= amount:
+            if denominations[coin] > 0:
+                change.append(coin)
+                denominations[coin] -= 1
+                amount -= coin
+            else:
+                break
+    if amount == 0:
+        return change
+    else:
+        raise Exception("Insufficient change available")
 
 # Test coin denominations
 test_are_equal(get_change(0), [])
@@ -27,7 +50,11 @@ test_are_equal(get_change(7), [5,2])
 test_are_equal(get_change(301), [100,100, 100, 1])
 test_are_equal(get_change(393), [100,100, 100, 50, 20, 20, 2, 1])
 
-# Test for specific requested coins
+# Test for specific requested coins with limited number
 test_are_equal(get_change(35, usd_coins), [25,10])
-
+try:
+    test_are_equal(get_change(9, usd_coins), [5,1,1,1,1])
+except Exception as E:
+    print(E)
+    
 print ("All tests pass!")
