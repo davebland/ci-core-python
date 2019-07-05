bbd873cb86e613a51311f6140cab70127693eb25
Ubuntu <ubuntu@ip-172-31-21-184.eu-west-1.compute.internal>
2019-07-05 20:02:48 +0000

    basic boggle solver game

---
+++ boggle_game/boggle.py
@@ -0,0 +1,67 @@
+from string import ascii_uppercase
+from random import choice
+
+def make_grid(width, height):
+    return {(row, col): choice(ascii_uppercase)
+        for row in range(height)
+        for col in range(width)
+    }
+
+def neighbours_of_position(coords):
+    row = coords[0]
+    col = coords[1]
+    
+    top_left = (row - 1, col - 1)
+    top_center = (row - 1, col)
+    top_right = (row - 1, col + 1)
+    left = (row, col - 1)
+    right = (row, col + 1)
+    bottom_left = (row + 1, col - 1)
+    bottom_center = (row + 1, col)
+    bottom_right = (row + 1, col + 1)
+    
+    return [top_left, top_center, top_right, left, right, bottom_left, bottom_center, bottom_right]
+    
+def all_grid_neighbours(grid):
+    neighbours = {}
+    for position in grid:
+        position_neighbours = neighbours_of_position(position)
+        neighbours[position] = [p for p in position_neighbours if p in grid]
+    return neighbours
+    
+def path_to_word(grid, path):
+    return ''.join([grid[p] for p in path])
+    
+def search(grid, dictionary):
+    neighbours = all_grid_neighbours(grid)
+    paths = []
+    
+    def do_search(path):
+        word = path_to_word(grid, path)
+        if word in dictionary:
+            paths.append(path)
+        for next_pos in neighbours[path[-1]]:
+            if next_pos not in path:
+                do_search(path + [next_pos])
+    
+    for position in grid:
+        do_search([position])
+    words = []
+    for path in paths:
+        words.append(path_to_word(grid, path))
+    return set(words)
+    
+def get_dictionary(filename):
+    with open(filename) as f:
+        return [w.strip().upper() for w in f]
+
+def main():
+    grid = make_grid(3,3)
+    dictionary = get_dictionary('bogwords.txt')
+    words = search(grid, dictionary)
+    for word in words:
+        print(word)
+    print("Found %s words" % len(words))
+    
+if __name__ == "__main__":
+    main()
